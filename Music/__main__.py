5b84f91c8dcdac4069c6666778c2a00cc2077d4b
namwg1708owmer <109025355+namwg1708owmer@users.noreply.github.com>
2022-07-27 20:29:09 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,25 +1,79 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from Music import LOGGER, app, userbot
 
 
+loop = asyncio.get_event_loop()
 
-import glob
-from pathlib import Path
-from music.utils import load_plugins
-import logging
-from music import music
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("Music").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("Music").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Music.plugins" + all_module)
+    LOGGER("music.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await Music.start()
+    try:
+        await Music.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("Music").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await Music.decorators()
+    LOGGER("Music").info("Music Bot Started Successfully")
+    await idle()
 
-path = "music/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        load_plugins(plugin_name.replace(".py", ""))
-    
-print("Successfully Started Bot!")
-print("Visit @MyBotsupport")
 
 if __name__ == "__main__":
-    music.run_until_disconnected()
+    loop.run_until_complete(init())
+    LOGGER("Music").info("Stopping Music Music Bot! GoodBye")
